dbab1ba230919e0e9ac63641d620aa8b1bf49523
Arthur Alonso <arthuralonso8356@gmail.com>
2026-05-17 23:55:17 -0300

    :sparkles: feat: Adicionando SearchTypeToggle component responsavel por permitir que o usuário defina uma entre duas opções

---
+++ taxas-de-juros-nextjs-app/src/app/ui/SearchTypeToggle.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useEffect, useRef } from "react";
+
+import { poppins } from "../layout";
+
+interface SearchTypeToggle {
+    leftSideText: string;
+    rightSideText: string;
+    onToggle: (selectedSide: string) => void
+}
+
+export default function SearchTypeToggle({ leftSideText, rightSideText, onToggle }: SearchTypeToggle) {
+
+    const sideMapping = {
+        0: leftSideText,
+        1: rightSideText
+    };
+
+    type keyMap = keyof typeof sideMapping;
+
+    let selectedOption = useRef<keyMap>(1);
+
+    const handleToggleOption = () => {
+        selectedOption.current = selectedOption.current ? 0 : 1;
+        onToggle(sideMapping[selectedOption.current])
+    }
+
+    useEffect(handleToggleOption, []);
+
+    return (
+        <figure className={`${poppins.className} w-full h-fit self-center flex items-center justify-center text-base capitalize`}>
+            <label className="inline-flex items-center cursor-pointer">
+                <span className="select-none font-medium text-heading">{leftSideText}</span>
+                <input onChange={handleToggleOption} type="checkbox" value={0} className="sr-only peer " />
+                <div className="relative mx-3 w-9 h-5 bg-[#7a004b] peer-focus:outline-none peer-focus:ring-3 peer-focus:ring-[#7a004b]/70 dark:peer-focus:ring-[#7a004b]/70 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-0.5 after:inset-s-0.5 after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-[#7a004b]"></div>
+                <span className="select-none font-medium text-heading">{rightSideText}</span>
+            </label>
+
+        </figure>
+    )
+}
