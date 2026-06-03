54bcab9e3ad53ea6cba279e32f047d6d36b668d3
Arthur Alonso <arthuralonso8356@gmail.com>
2026-06-03 16:54:37 -0300

    :truck: chore: Renomeando para deixar mais intuitivo a funcionalidade do componente

--- taxas-de-juros-nextjs-app/src/app/ui/SearchTypeToggleSwitch.tsx
+++ taxas-de-juros-nextjs-app/src/app/ui/SearchTypeToggleSwitch.tsx
@@ -3,27 +3,23 @@
 import { useEffect, useRef } from "react";
 
 import { poppins } from "../layout";
+import { searchTypeToggleSwitchMapping } from "@/src/web/constants";
 
-interface SearchTypeToggle {
-    leftSideText: string;
-    rightSideText: string;
+interface SearchTypeToggleSwitch {
+    textOnLeftSide: string;
+    textOnRightSide: string;
     onToggle: (selectedSide: string) => void
 }
 
-export default function SearchTypeToggle({ leftSideText, rightSideText, onToggle }: SearchTypeToggle) {
+export default function SearchTypeToggleSwitch({ textOnLeftSide, textOnRightSide, onToggle }: SearchTypeToggleSwitch) {
 
-    const sideMapping = {
-        0: leftSideText,
-        1: rightSideText
-    };
+    type keyMap = keyof typeof searchTypeToggleSwitchMapping;
 
-    type keyMap = keyof typeof sideMapping;
-
-    let selectedSide = useRef<keyMap>(1);
+    let selectedSide = useRef(1);
 
     const handleChange = () => {
         selectedSide.current = selectedSide.current ? 0 : 1;
-        onToggle(sideMapping[selectedSide.current])
+        onToggle(searchTypeToggleSwitchMapping[Object.keys(searchTypeToggleSwitchMapping)[selectedSide.current] as keyMap])
     }
 
     useEffect(handleChange, []);
@@ -31,10 +27,10 @@ export default function SearchTypeToggle({ leftSideText, rightSideText, onToggle
     return (
         <figure className={`${poppins.className} w-full h-fit self-center flex items-center justify-center text-base capitalize`}>
             <label className="inline-flex items-center cursor-pointer">
-                <span className="select-none font-medium text-heading">{leftSideText}</span>
+                <span className="select-none font-medium text-heading">{textOnLeftSide}</span>
                 <input onChange={handleChange} type="checkbox" value={0} className="sr-only peer" />
                 <div className="relative mx-3 w-9 h-5 bg-[#7a004b] peer-focus:outline-none peer-focus:ring-3 peer-focus:ring-[#7a004b]/70 dark:peer-focus:ring-[#7a004b]/70 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-0.5 after:inset-s-0.5 after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-[#7a004b]"></div>
-                <span className="select-none font-medium text-heading">{rightSideText}</span>
+                <span className="select-none font-medium text-heading">{textOnRightSide}</span>
             </label>
 
         </figure>
