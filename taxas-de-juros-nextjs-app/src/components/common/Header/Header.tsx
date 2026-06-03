4a8a99361426c51556190ce6398506cd37fb224f
Arthur Alonso <arthuralonso8356@gmail.com>
2026-06-03 16:50:33 -0300

    :truck: chore: Movendo para src/componenets/Header com intutuito de utilizar uma arquitetura de projeto mais recomendada

--- taxas-de-juros-nextjs-app/src/components/common/Header/Header.tsx
+++ taxas-de-juros-nextjs-app/src/components/common/Header/Header.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import LogoProcon from '../../../public/HeaderLogo.png';
-import { poppins } from "../layout";
+import HeaderLogo from "@/public/images/HeaderLogo.png";
+import { poppins } from "@/src/app/layout";
 import HeaderNavigation from "./HeaderNavigation";
 
 export default function Header({ title }: { title: string }) {
     return (
         <header className={`${poppins.className} bg-[#7a004b] h-48 w-full flex flex-col `}>
             <figure className="w-full h-60/100 flex items-center justify-center">
-                <Image src={LogoProcon} alt="Logo do Procon" className="w-fit h-full object-contain self-end" />
+                <Image src={HeaderLogo} alt="Logo do Procon" className="w-fit h-full object-contain self-end" />
             </figure>
             <nav className="w-full h-20/100 mt-auto bg-[#7a004b]  flex items-start justify-end pb-1.5 pr-1.5">
                 <HeaderNavigation />
