eaee6127f33c767034d84b95b68fefe9bd087dac
Arthur Alonso <arthuralonso8356@gmail.com>
2026-06-06 00:02:10 -0300

    :wrench: chore(scraper): Tipagem utiizados no scraper

---
+++ taxas-de-juros-nextjs-app/src/types/data-scraper.types.ts
@@ -0,0 +1,18 @@
+export interface DataScraperError {
+    type: 'targetError' | 'connectionError';
+    errorTarget?: string;
+}
+
+interface DataScraperSucess {
+    periods: string[];
+    modalities: string[];
+    averageInterestRates: string[];
+    interestRatePeriod: string[];
+}
+
+export interface DataScraperResult {
+    passed?: DataScraperSucess;
+    failed?: DataScraperError[];
+}
+
+export type DataScraperType = { sucess: true, result: DataScraperResult } | { sucess: false, error: DataScraperError }
