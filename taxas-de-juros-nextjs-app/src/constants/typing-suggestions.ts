f8aaac79e9c9508339df7ea5d4a729cbb798f5b7
Arthur Alonso <arthuralonso8356@gmail.com>
2026-06-07 19:53:43 -0300

    :bulb: chore(constants): Adicionando lista de sugestões padrão de digitação

---
+++ taxas-de-juros-nextjs-app/src/constants/typing-suggestions.ts
@@ -0,0 +1,9 @@
+export const TYPING_SUGGESTIONS: string[] = [
+    'Cartão de crédito parcelado',
+    'Cartão de crédito rotativo',
+    'Cheque especial',
+    'Crédito pessoal consignado INSS',
+    'Crédito pessoal consignado privado',
+    'Crédito pessoal consignado público',
+    'Crédito pessoal não consignado'
+] as const;
