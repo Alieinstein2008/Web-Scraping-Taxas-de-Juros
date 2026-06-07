be639d906a2e84b01c3a2ec39b0e3b3c1a5fddd1
Arthur Alonso <arthuralonso8356@gmail.com>
2026-06-07 20:04:48 -0300

    :globe_with_meridians: feat(Routes): Adicionando rota POST para lidar com a busca de dados do scraper pelo cliente.

---
+++ taxas-de-juros-nextjs-app/src/app/api/scraper/route.ts
@@ -0,0 +1,22 @@
+import { interestRateDataScraper } from '@/src/services/scraper/scraper';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+    try {
+        const { selectedPeriod, searchTargets } = await request.json();
+
+        if (!selectedPeriod || !Array.isArray(searchTargets)) {
+            return NextResponse.json({ error: 'Parâmetros inválidos' }, { status: 400 });
+        }
+
+        const data = await interestRateDataScraper({
+            searchTargets: searchTargets,
+            selectedPeriod: selectedPeriod
+        });
+
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error('Erro no POST do scraper:', error);
+        return NextResponse.json({ error: 'Falha ao processar scraper' }, { status: 500 });
+    }
+}
